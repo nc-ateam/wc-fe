0fb2bd5f27d276479aab048cc6e86b56e24239ed
willtaylor1494 <willtaylor1494@gmail.com>
2018-08-15 12:12:07 +0100

    adds button, graph, text report, report field

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReportField from './ReportField';
 
 class Home extends Component {
   state = {}
@@ -6,6 +7,7 @@ class Home extends Component {
     return (
       <div>
         <p>Rosie</p>
+        <ReportField />
       </div>
     );
   }
