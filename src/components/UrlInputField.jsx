3b357f5613090fa33765fa811222b23b8b22d455
rzhekova <rzhekova@gmail.com>
2018-08-15 11:51:29 +0100

    creates url input field component

---
+++ src/components/UrlInputField.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UrlInputField extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h3>add url in the form below:</h3>
+      </div>
+    );
+  }
+}
+
+export default UrlInputField;
