ef1a8098185dc35e2fab1c92867e9b451aa4e8a7
willtaylor1494 <willtaylor1494@gmail.com>
2018-08-15 11:14:06 +0100

    creates Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Heading from "./components/Heading";
+import Home from "./components/Home";
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Heading />
+        <Route path='/' component={Home} />
+        {/* Route to path /report here */}
       </div>
     );
   }
